a64ab3d9eb3f3a71f14eb3b64efba6a820d1a391
Olivier Scherrer <pode.fr@gmail.com>
2014-10-25 23:40:15 -0400

    add index and test

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @license data-binding-object-observe https://github.com/...tbd..
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
+ */
+"use strict";
+
